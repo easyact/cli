eea1a71f40f79e58e07a2b79727e23efaf9a82d9
zhaolei <34zhaolei@sina.com>
2016-12-01 20:51:01 +0800

    rollback to js

---
+++ server/models/projects.ts
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var projectSchema = new mongoose.Schema({
+    name: {type: String, default: 'test', required: true},
+    joiners: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
+  },
+  {strict: false});
+mongoose.model('Project', projectSchema);
+
+var Project = mongoose.model('Project');
--- server/routes/public.ts
+++ server/routes/public.ts
@@ -1,12 +1,15 @@
 import {Router, Response, Request} from 'express';
+import mongoose from 'mongoose';
 
 const r: Router = Router();
-
+const Project = mongoose.model('Project');
 r.get('/projects.json', (request: Request, response: Response) => {
-  response.json([{
+  let list = [{
     title: 'Greetings.',
     desc: 'Hello Angular 2'
-  }]);
+  }];
+  Project.find
+  response.json(list);
 });
 r.post('/projects.json', (req, res) => {
   res.json({id: 1});
